93cfb8673b262e35c8ed46c3efea3b55bca35499
James <11054922+entrancedjames@users.noreply.github.com>
2023-08-13 13:57:03 -0700

    make gamble a querable id

--- config/defaults.ts
+++ config/defaults.ts
@@ -8,7 +8,7 @@ export const feeDenom = 'ujunox';
 // export const cw20ContractAddress = 'wasm1p7vmrhl3s0fyl0m9hk2hlm7uuxq84hztur63n8ryh85chh30vt6q89shcv';
 // export const CW_LOTTO_ADDRESS = 'juno1w4lgq9mvlvtldf0q9thqtaq986tuhy3j7jneq68lc4xhecvaa9aq8mzqhc';
 
-export const CW_LOTTO_ADDRESS = 'juno1qdsm700ql6cpl5wsxarvwzst44yjlkadvumu6xgjfl8fdyrhqxvqt5xumz';
+export const CW_LOTTO_ADDRESS = 'juno1law3szlnn4snqe6gylgt3h7r003e549fjs9sl9tf6zh3w480e33shlqrlp';
 
 export const chainassets: AssetList = assets.find(
   (chain) => chain.chain_name === chainName
